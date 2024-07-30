41845dedb497e5ebaf45fac1209168ea8021faee
Kitts Rey Rubio <kittr64@gmail.com>
2024-07-30 18:47:11 +0800

    fixed expanded sensor node card
    merge conflict tings >:(

--- lib/components/devices/sensor_node_card_expanded.dart
+++ lib/components/devices/sensor_node_card_expanded.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<<< HEAD:lib/components/devices/details_card.dart
-<<<<<<<< HEAD:lib/components/devices/sensor_node_card_expanded.dart
 import 'package:smmic/models/device_data_models.dart';
 import 'package:smmic/services/datetime_formatting.dart';
 import 'package:smmic/services/devices/sensor_data.dart';
-========
-import 'package:smmic/services/datetime_formatting.dart';
->>>>>>>> 18d9102da2af79d20a0c1a725e14a976a0241be5:lib/components/devices/details_card.dart
-========
-import 'package:smmic/models/device_data_models.dart';
-import 'package:smmic/services/datetime_formatting.dart';
-import 'package:smmic/services/devices/sensor_data.dart';
->>>>>>>> backup:lib/components/devices/sensor_node_card_expanded.dart
 import 'package:smmic/subcomponents/devices/battery_level.dart';
 import 'package:smmic/subcomponents/devices/gauge.dart';
 
@@ -23,14 +13,10 @@ class SensorNodeCardExpanded extends StatefulWidget {
   @override
   State<SensorNodeCardExpanded> createState() => _SensorNodeCardExpandedState();
 }
-
-<<<<<<<< HEAD:lib/components/devices/details_card.dart
-<<<<<<<< HEAD:lib/components/devices/sensor_node_card_expanded.dart
-========
->>>>>>>> backup:lib/components/devices/sensor_node_card_expanded.dart
 class _SensorNodeCardExpandedState extends State<SensorNodeCardExpanded> {
 
   late SensorNodeSnapshot _sensorNodeSnapshot;
+  DatetimeFormatting _dateTimeFormatting = DatetimeFormatting();
 
   @override
   void initState() {
@@ -38,15 +24,6 @@ class _SensorNodeCardExpandedState extends State<SensorNodeCardExpanded> {
     //TODO: assign proper id variable for 'getSnapshot', preferably move this out of the initState() function too
     _sensorNodeSnapshot = SensorNodeDataServices().getSnapshot(widget.deviceID);
   }
-<<<<<<<< HEAD:lib/components/devices/details_card.dart
-========
-class _DetailsCardState extends State<DetailsCard> {
->>>>>>>> 18d9102da2af79d20a0c1a725e14a976a0241be5:lib/components/devices/details_card.dart
-========
->>>>>>>> backup:lib/components/devices/sensor_node_card_expanded.dart
-
-  final DatetimeFormatting _dateTimeFormatting = DatetimeFormatting();
-  // final SensorNodeDataServices _sensorDataServices = SensorNodeDataServices();
 
   @override
   Widget build(BuildContext context) {
@@ -56,75 +33,75 @@ class _DetailsCardState extends State<DetailsCard> {
       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
       height: 450,
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.06),
-            spreadRadius: 0,
-            blurRadius: 4,
-            offset: Offset(0, 4)
-          )
-        ]
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.06),
+                spreadRadius: 0,
+                blurRadius: 4,
+                offset: Offset(0, 4)
+            )
+          ]
       ),
       child: Column(
         children: [
           Expanded(
-            flex: 1,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  BatteryLevel(
-                    batteryLevel: _sensorNodeSnapshot.batteryLevel.toInt(),
-                    alignmentAdjust: 1,
-                    shrinkPercentSign: false
-                  ),
-                  Text(
-                    _dateTimeFormatting.formatTime(_sensorNodeSnapshot.timestamp),
-                    style: const TextStyle(fontFamily: 'Inter', fontSize: 20)
-                  )
-                ],
-              ),
-            )
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    BatteryLevel(
+                        batteryLevel: _sensorNodeSnapshot.batteryLevel.toInt(),
+                        alignmentAdjust: 1,
+                        shrinkPercentSign: false
+                    ),
+                    Text(
+                        _dateTimeFormatting.formatTime(_sensorNodeSnapshot.timestamp),
+                        style: const TextStyle(fontFamily: 'Inter', fontSize: 20)
+                    )
+                  ],
+                ),
+              )
           ),
           Expanded(
             flex: 4,
             child: RadialGauge(
-              valueType: 'soilMoisture',
-              value: _sensorNodeSnapshot.soilMoisture,
-              limit: 100,
-              scaleMultiplier: 1.5
+                valueType: 'soilMoisture',
+                value: _sensorNodeSnapshot.soilMoisture,
+                limit: 100,
+                scaleMultiplier: 1.5
             ),
           ),
           Expanded(
-            flex: 3,
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 160,
-                    child: RadialGauge(
-                      valueType: 'temperature',
-                      value: _sensorNodeSnapshot.temperature,
-                      limit: 100,
-                      radiusMultiplier: 0.9,
+              flex: 3,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 160,
+                      child: RadialGauge(
+                        valueType: 'temperature',
+                        value: _sensorNodeSnapshot.temperature,
+                        limit: 100,
+                        radiusMultiplier: 0.9,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 160,
-                    child: RadialGauge(
-                      valueType: 'humidity',
-                      value: _sensorNodeSnapshot.humidity,
-                      limit: 100,
-                      radiusMultiplier: 0.9,
+                    SizedBox(
+                      width: 160,
+                      child: RadialGauge(
+                        valueType: 'humidity',
+                        value: _sensorNodeSnapshot.humidity,
+                        limit: 100,
+                        radiusMultiplier: 0.9,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            )
+                  ],
+                ),
+              )
           )
         ],
       ),
