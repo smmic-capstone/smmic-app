bc9d30fe5a6a8be1b87cbf212c5492ab8690dde3
Kitts Rey Rubio <kittr64@gmail.com>
2024-11-19 02:40:52 +0800

    animations, sink node card
    - added animation to fade out and fade in the weather widget when sink node list is scrolled

--- lib/components/devices/cards/sink_node_card.dart
+++ lib/components/devices/cards/sink_node_card.dart
@@ -1,8 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:smmic/models/device_data_models.dart';
-import 'package:smmic/pages/devices_subpages/sensor_node_subpage.dart';
 import 'package:smmic/providers/theme_provider.dart';
 import 'package:smmic/subcomponents/devices/battery_level.dart';
 import 'package:smmic/subcomponents/devices/device_dialog.dart';
@@ -18,93 +19,32 @@ class SinkNodeCard extends StatefulWidget {
 }
 
 class _SinkNodeCardState extends State<SinkNodeCard> {
+
   @override
   Widget build(BuildContext context) {
-    final DeviceDialog _skDeviceDialog = DeviceDialog(
-        context: context,
-        deviceID: widget.deviceInfo.deviceID,
-        latitude: widget.deviceInfo.latitude,
-        longitude: widget.deviceInfo.longitude);
+    return _cardBackground();
+  }
 
-    return GestureDetector(
-      //TODO: IMPLEMENT ON TAP FUNCTION
-      // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) {
-      //   return SinkNode(deviceID: widget.deviceData.deviceID, deviceName: widget.deviceData.deviceName);
-      // })),
+  Widget _cardBackground() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 25),
       child: Stack(
         children: [
-          Container(
-              margin: const EdgeInsets.only(left: 25, right: 25, bottom: 15),
-              padding: const EdgeInsets.symmetric(horizontal: 19, vertical: 18),
-              decoration: BoxDecoration(
-                  color: context.watch<UiProvider>().isDark
-                      ? Colors.black
-                      : Colors.white,
-                  borderRadius: const BorderRadius.all(Radius.circular(15)),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.black.withOpacity(0.06),
-                        spreadRadius: 0,
-                        blurRadius: 4,
-                        offset: const Offset(0, 4))
-                  ]),
-              height: 160,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Column(
-                      children: [
-                        Expanded(
-                            flex: 3,
-                            child: DeviceName(
-                                deviceName: widget.deviceInfo.deviceName)),
-                        Expanded(
-                            flex: 1,
-                            //TODO: add snapshot data here
-                            child: BatteryLevel(batteryLevel: 00))
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Container(),
-                  ),
-                ],
-              )),
-          Container(
-            padding: const EdgeInsets.only(right: 37, top: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                RotatedBox(
-                  quarterTurns: 2,
-                  child: IconButton(
-                    icon: const Icon(
-                      CupertinoIcons.pencil_circle,
-                      size: 20,
-                    ),
-                    color: context.watch<UiProvider>().isDark
-                        ? Colors.white
-                        : Colors.black,
-                    onPressed: () {
-                      _skDeviceDialog.renameDialog();
-                    },
-                    padding: EdgeInsets.zero,
-                    constraints: const BoxConstraints(),
+          ClipRRect(
+            borderRadius: const BorderRadius.all(
+                Radius.circular(25)
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaY: 10, sigmaX: 10),
+              child: Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.65),
+                  borderRadius: const BorderRadius.all(
+                      Radius.circular(25)
                   ),
                 ),
-                RotatedBox(
-                  quarterTurns: 2,
-                  child: Icon(
-                    CupertinoIcons.arrow_down_left_circle,
-                    size: 20,
-                    color: context.watch<UiProvider>().isDark
-                        ? Colors.white
-                        : Colors.black,
-                  ),
-                ),
-              ],
+              ),
             ),
           )
         ],
